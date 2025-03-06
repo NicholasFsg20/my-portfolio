45f5a0343cf6ed7ea4583fa1615e047140c03fd4
Nicholas Souza <nicholasfsg@gmail.com>
2025-03-05 21:06:19 -0300

    feat: creates hero components UI

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,39 +1,52 @@
-import { Button, Container, Grid2, styled, Typography } from "@mui/material"
+import { Box, Container, Grid2, styled, Typography } from "@mui/material"
 import DownloadIcon from '@mui/icons-material/Download';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import EmailIcon from '@mui/icons-material/Email';
+import StyledButton from "../../../../components/StyledButton/StyledButton";
+import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
 
 const Hero = () => {
 
-    const StyledHero = styled("div")(() => ({
-        backgroundColor: "black",
-        height: "100vh"
+    const StyledHero = styled("div")(({ theme }) => ({
+        backgroundColor: theme.palette.primary.main,
+        height: "100vh",
+        display: "flex",
+        alignItems: "center"
     }))
 
     return (
         <>
             <StyledHero>
                 <Container maxWidth="lg">
-                    <Grid2 container spacing={2}>
-                        <Grid2 size={{ xs: 12, md: 8 }}>
-                            <Typography color="primary" variant="h1" textAlign="center">Nicholas Ferreira Souza Gomes</Typography>
-                            <Typography color="primary" variant="h2" textAlign="center">I'm a Copmuter Engineer student</Typography>
-                            <Grid2 container display="flex" justifyContent="center">
-                                <Grid2 size={{ xs: 12, md: 4 }} display="flex" justifyContent="center">
-                                    <Button>
-                                        <DownloadIcon />
-                                        Download CV
-                                    </Button>
+                    <Grid2>
+                        <Grid2 size={{ xs: 12, md: 7 }}>
+                            <Box position="relative">
+                            <Grid2>
+                                <Box position="absolute" width={"60%"} top={-200} right={180}>
+                                    <img src="" alt="" />
+                                    <AnimatedBackground />
+                                </Box>
                                 </Grid2>
-                                <Grid2 size={{ xs: 12, md: 4 }} display="flex" justifyContent="center">
-                                    <Button>
-                                        <MailOutlineIcon />
-                                        Contact me
-                                    </Button>
+                                <Grid2 justifyContent="center">
+                                <Box position="relative" >
+                                    <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Nicholas Gomes</Typography>
+                                    <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm a Copmuter Engineer student</Typography>
+                                    <Grid2 container display="flex" justifyContent="center" spacing={3}  pt={3}>
+                                        <Grid2 size={{ xs: 12, md: 4 }} display="flex" justifyContent="center">
+                                            <StyledButton>
+                                                <DownloadIcon />
+                                                <Typography>Download CV</Typography>
+                                            </StyledButton>
+                                        </Grid2>
+                                        <Grid2 size={{ xs: 12, md: 4 }} display="flex" justifyContent="center">
+                                            <StyledButton>
+                                                <EmailIcon />
+                                                <Typography>Contact me</Typography>
+                                            </StyledButton>
+                                        </Grid2>
+                                    </Grid2>
+                                </Box>
                                 </Grid2>
-                            </Grid2>
-                        </Grid2>
-                        <Grid2 size={{ xs: 12, md: 4 }}>
-                            size=4
+                            </Box>
                         </Grid2>
                     </Grid2>
                 </Container>
